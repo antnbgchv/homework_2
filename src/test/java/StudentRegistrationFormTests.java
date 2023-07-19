2a602fae3ef69ad7c63e4acf2b01d45baa4e6f0f
bogachev <antn.bgchv@gmail.com>
2023-07-19 10:53:25 +0300

    made changes to the code based on the teacher's comment

--- src/test/java/StudentRegistrationFormTests.java
+++ src/test/java/StudentRegistrationFormTests.java
@@ -2,18 +2,17 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class StudentRegistrationFormTests {
 
     @BeforeAll
     static void config() {
-        //конфигурационные параметры (соображал как открывать браузер фуллскрин, пока не нашел понятное решение)
+        //конфигурационные параметры
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
@@ -23,6 +22,10 @@ public class StudentRegistrationFormTests {
     void fillRegistrationFormTest() {
 
         open("/automation-practice-form");
+
+        //добавил код, который скрывает рекламу и футер
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
 
         //задаем значения строкам
         $("#firstName").setValue("Homer");
@@ -37,42 +40,50 @@ public class StudentRegistrationFormTests {
         $(".react-datepicker__year-select").selectOptionContainingText("1956");
         $(".react-datepicker__day.react-datepicker__day--012").click();
 
-        /* выбор subject'а (не получалось просто с #subjectsContainer, нашел информацию,
-        что нужно добавить input, мысли зачем это есть, но понимания нет, объясните пожалуйста!) */
+        // выбор subject
         $("#subjectsContainer input").setValue("Physics").pressEnter();
 
         //выбор хобби
         $("#hobbiesWrapper").$(byText("Sports")).click();
 
         //загрузка изображения
-        $("#uploadPicture").uploadFile(new File("src/test/resources/coolPic.jpg"));
+        $("#uploadPicture").uploadFromClasspath("coolPic.jpg");
 
         //ввод текущего адреса
         $("#currentAddress").setValue("742 Evergreen Terrace");
 
-        //выбор штата и города (уверен эти строки можно оптимизировать)
-        $(byText("Select State")).click();
-        $(byText("Haryana")).click();
-        $(byText("Select City")).click();
-        $(byText("Karnal")).click();
+        //выбор штата и города
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Karnal")).click();
 
         //отправка формы
         $("#submit").click();
 
-        /* тесты (справедливости ради считаю эти тесты некорректными, но разобраться как
-        искать элемент по тексту, а потом переходить на parent или sibling и т.д. не удалось */
-        $(".table").shouldHave(
-                text("Homer Simpson"),
-                text("mr.plow@gmail.com"),
-                text("Male"),
-                text("1234567890"),
-                text("12 May,1956"),
-                text("Physics"),
-                text("Sports"),
-                text("coolPic.jpg"),
-                text("742 Evergreen Terrace"),
-                text("Haryana Karnal")
-        );
+        /* тесты (добавил проверку что форма отобразилась на экране после сабмита и изменил
+        сами тесты) */
+        $(".modal-content").should(appear);
+        $(".table-responsive").$(byText("Student Name"))
+                .parent().shouldHave(text("Homer Simpson"));
+        $(".table-responsive").$(byText("Student Email"))
+                .parent().shouldHave(text("mr.plow@gmail.com"));
+        $(".table-responsive").$(byText("Gender"))
+                .parent().shouldHave(text("Male"));
+        $(".table-responsive").$(byText("Mobile"))
+                .parent().shouldHave(text("1234567890"));
+        $(".table-responsive").$(byText("Date of Birth"))
+                .parent().shouldHave(text("12 May,1956"));
+        $(".table-responsive").$(byText("Subjects"))
+                .parent().shouldHave(text("Physics"));
+        $(".table-responsive").$(byText("Hobbies"))
+                .parent().shouldHave(text("Sports"));
+        $(".table-responsive").$(byText("Picture"))
+                .parent().shouldHave(text("coolPic.jpg"));
+        $(".table-responsive").$(byText("Address"))
+                .parent().shouldHave(text("742 Evergreen Terrace"));
+        $(".table-responsive").$(byText("State and City"))
+                .parent().shouldHave(text("Haryana Karnal"));
 
     }
 }
